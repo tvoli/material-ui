f81d50f181cde24f0af94466055347ea05572733
x1a0 <zhangxiao@xzhang.net>
2015-07-06 16:25:37 +0200

    Change auto-prefix to prefix for all vendors
    So server and client can generate same inline css code which works with
    isomorphic React rendering.

--- src/styles/auto-prefix.js
+++ src/styles/auto-prefix.js
@@ -6,7 +6,10 @@ module.exports = {
   all: function(styles) {
     var prefixedStyle = {};
     for (var key in styles) {
-      prefixedStyle[this.single(key)] = styles[key];
+      var prefixed = this.singleAll(key);
+      for (var index in prefixed) {
+        prefixedStyle[prefixed[index]] = styles[key];
+      }
     }
     return prefixedStyle;
   },
@@ -17,6 +20,19 @@ module.exports = {
 
   single: function(key) {
     return isBrowser ? Modernizr.prefixed(key) : key;
+  },
+
+  // prefix for all vendors
+  singleAll: function test(key) {
+    var toPrefix = {
+      'borderRadius': ['borderRadius', 'WebkitBorderRadius', 'MozBorderRadius', 'OBorderRadius', 'msBorderRadius'],
+      'boxShadow':    ['boxShadow',    'WebkitBoxShadow',    'MozBoxShadow',    'OBoxShadow',    'msBoxShadow'   ],
+      'opacity':      ['opacity',      'WebkitOpacity',      'MozOpacity',      'OOpacity',      'msOpacity'     ],
+      'transform':    ['transform',    'WebkitTransform',    'MozTransform',    'OTransform',    'msTransform'   ],
+      'transition':   ['transition',   'WebkitTransition',   'MozTransition',   'OTransition',   'msTransition'  ]
+    };
+
+    return toPrefix[key] ? toPrefix[key] : [key];
   },
 
   singleHyphened: function(key) {
